96b6bd7443d29d74467f4484e42f4755a472dd8a
Sabouni Youssef <youssefsabouni@icloud.com>
2021-05-29 07:14:15 +0200

    first commit

---
+++ src/test/java/com/safetynet/safetynetalerts/SafetyNetAlertsApplicationTests.java
@@ -0,0 +1,14 @@
+package com.safetynet.safetynetalerts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafetyNetAlertsApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
