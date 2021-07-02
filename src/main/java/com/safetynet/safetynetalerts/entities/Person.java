6795ea90a7d1f5f71417f7a5c4f5e9e4fa830bab
Sabouni Youssef <youssefsabouni@icloud.com>
2021-07-02 15:19:28 +0200

    feature(Person): override hashcode and equals methods

--- src/main/java/com/safetynet/safetynetalerts/entities/Person.java
+++ src/main/java/com/safetynet/safetynetalerts/entities/Person.java
@@ -103,4 +103,41 @@ public class Person {
 
         this.medicalRecord = medicalRecord;
     }
+
+    @Override
+    public int hashCode() {
+
+        return this.firstName.hashCode() * this.getLastName().hashCode() * this.email.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+
+        if (this == o) {
+
+            return true;
+        }
+
+        if (o == null) {
+
+            return false;
+        }
+
+        if (this.getClass() != o.getClass()) {
+
+            return false;
+        }
+
+        Person person = (Person) o;
+
+        return this.firstName.equalsIgnoreCase(person.firstName)
+               && (this.lastName.equalsIgnoreCase(person.lastName)
+                   && email.equalsIgnoreCase(person.email));
+    }
+
+    @Override
+    public String toString() {
+
+        return getLastName() + " " + getFirstName() + " " + getEmail();
+    }
 }
