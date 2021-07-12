c1565ccabc5bfb8360f20fad13e5e7bf956ea315
Sabouni Youssef <youssefsabouni@icloud.com>
2021-07-12 02:55:15 +0200

    feature(MedicalRecord): push MedicalRecord entity

---
+++ src/main/java/com/safetynet/safetynetalerts/entities/MedicalRecord.java
@@ -0,0 +1,40 @@
+package com.safetynet.safetynetalerts.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MedicalRecord {
+
+    private List<String> medications = new ArrayList<>();
+    private List<String> allergies   = new ArrayList<>();
+
+    public MedicalRecord() {
+
+    }
+
+    public MedicalRecord(List<String> medications, List<String> allergies) {
+
+        this.medications = medications;
+        this.allergies   = allergies;
+    }
+
+    public List<String> getMedications() {
+
+        return medications;
+    }
+
+    public void setMedications(List<String> medications) {
+
+        this.medications = medications;
+    }
+
+    public List<String> getAllergies() {
+
+        return allergies;
+    }
+
+    public void setAllergies(List<String> allergies) {
+
+        this.allergies = allergies;
+    }
+}
