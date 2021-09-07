fbcf0e0e839074c175a16247abe10a6a6bb3b054
Sabouni Youssef <youssefsabouni@icloud.com>
2021-09-08 01:05:58 +0200

    feature(FireStationServiceTest): create test for FireStationService

--- src/test/java/com/safetynet/safetynetalerts/services/FireStationServiceTest.java
+++ src/test/java/com/safetynet/safetynetalerts/services/FireStationServiceTest.java
@@ -11,6 +11,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -21,10 +22,51 @@ import static org.mockito.Mockito.*;
 @ExtendWith(MockitoExtension.class)
 public class FireStationServiceTest {
 
+    List<FireStation> fireStations = new ArrayList<>();
+
     @Mock
     private FireStationRepository fireStationRepository;
 
     private FireStationService fireStationService;
+
+    {
+        fireStations.addAll(
+                Arrays.asList(
+                        new FireStation() {{
+                            this.setStation(1L);
+                            this.setAddress("address1");
+                        }},
+                        new FireStation() {{
+                            this.setStation(1L);
+                            this.setAddress("address1");
+                        }},
+                        new FireStation() {{
+                            this.setStation(1L);
+                            this.setAddress("address1");
+                        }},
+                        new FireStation() {{
+                            this.setStation(1L);
+                            this.setAddress("address1");
+                        }},
+                        new FireStation() {{
+                            this.setStation(1L);
+                            this.setAddress("address1");
+                        }},
+                        new FireStation() {{
+                            this.setStation(1L);
+                            this.setAddress("address1");
+                        }},
+                        new FireStation() {{
+                            this.setStation(1L);
+                            this.setAddress("address1");
+                        }},
+                        new FireStation() {{
+                            this.setStation(1L);
+                            this.setAddress("address1");
+                        }}
+                )
+        );
+    }
 
     @BeforeEach
     public void setUpPerTest() {
@@ -35,16 +77,7 @@ public class FireStationServiceTest {
     @Test
     public void findAll_shouldReturnAllFireStationOfDatabase() {
 
-        when(fireStationRepository.findAll()).thenReturn(new ArrayList<>(Arrays.asList(
-                new FireStation(),
-                new FireStation(),
-                new FireStation(),
-                new FireStation(),
-                new FireStation(),
-                new FireStation(),
-                new FireStation(),
-                new FireStation()
-        )));
+        when(fireStationRepository.findAll()).thenReturn(new ArrayList<>());
 
         assertThat(fireStationService.findAll()).isEqualTo(fireStationRepository.findAll());
     }
@@ -60,7 +93,7 @@ public class FireStationServiceTest {
 
         FireStation fireStationUpdated = fireStationService.update(fireStation);
 
-        verify(fireStationRepository, times(1)).save(fireStation);
+        verify(this.fireStationRepository, times(1)).save(fireStation);
 
         assertThat(fireStationUpdated).isEqualTo(fireStation);
     }
@@ -71,28 +104,43 @@ public class FireStationServiceTest {
         FireStation fireStation = new FireStation();
         fireStation.setAddress("1509 Culver St");
 
-        assertThrows(ResponseStatusException.class, () -> fireStationService.update(fireStation));
-        verify(fireStationRepository, times(1)).findByAddress(anyString());
+        assertThrows(ResponseStatusException.class, () -> this.fireStationService.update(fireStation));
+        verify(this.fireStationRepository, times(1)).findByAddress(anyString());
     }
 
     @Test
-    public void delete_shouldRemoveTheFireStationFromTheDatabase_ForFireStationExisting() {
+    public void delete_shouldRemoveTheFireStationFromTheDatabase_forFireStationExisting() {
 
         FireStation fireStation = new FireStation();
         fireStation.setAddress("1509 Culver St");
 
-        when(fireStationRepository.findByAddress(anyString())).thenReturn(Optional.of(fireStation));
+        when(this.fireStationRepository.findByAddress(anyString())).thenReturn(Optional.of(fireStation));
 
         fireStationService.delete(fireStation.getAddress());
 
-        verify(fireStationRepository, times(1)).findByAddress(anyString());
+        verify(this.fireStationRepository, times(1)).findByAddress(anyString());
     }
 
     @Test
-    public void delete_shouldThrowNoSuchElementException_ForFireStationNotExisting() {
+    public void delete_shouldThrowNoSuchElementException_forFireStationNotExisting() {
 
-        when(fireStationRepository.findByAddress(anyString())).thenReturn(Optional.empty());
+        when(this.fireStationRepository.findByAddress(anyString())).thenReturn(Optional.empty());
 
         assertThrows(ResponseStatusException.class, () -> fireStationService.delete(""));
+    }
+
+    @Test
+    public void save_shouldReturnFireStationSaved_forNewFireStation() {
+
+        FireStation fireStation = new FireStation();
+        fireStation.setStation(999L);
+        fireStation.setAddress("rue des coquelicots");
+
+        when(this.fireStationRepository.isExisting(any(FireStation.class))).thenReturn(false);
+        when(this.fireStationRepository.save(any(FireStation.class))).thenReturn(fireStation);
+
+        FireStation fireStationSaved = this.fireStationService.save(fireStation);
+
+        assertThat(fireStationSaved).isEqualTo(fireStation);
     }
 }
